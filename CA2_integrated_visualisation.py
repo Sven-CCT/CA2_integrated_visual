7b46f9b6734f6ea92b2ca3e0dc0b0633a588d635
Sven-CCT <sba25167@student.cct.ie>
2026-05-28 12:44:27 +0100

    Update CA2_integrated_visualisation.py

--- CA2_integrated_visualisation.py
+++ CA2_integrated_visualisation.py
@@ -4,3 +4,12 @@ import numpy as np
 
 st.title("Retail Dashboard Exploration")
 
+DATE_COLUMN = 'Date'
+
+def load_data(nrows):
+    data=pd.read_csv("Groceries_dataset.csv", nrows=nrows)
+
+    lowercase=lambda x:str(x).lower
+    data.rename(lowercase, axis="columns", inplace=True)
+    data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
+    return data
